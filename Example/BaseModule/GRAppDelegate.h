ca5fdc604ac1507ef36f7661e48dddf0f47d553f
东哥 <1049646716@qq.com>
2020-02-07 16:03:36 +0800

    Initial commit

---
+++ Example/BaseModule/GRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GRAppDelegate.h
+//  BaseModule
+//
+//  Created by 1049646716@qq.com on 02/07/2020.
+//  Copyright (c) 2020 1049646716@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
